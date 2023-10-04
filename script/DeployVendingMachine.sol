d0c5ded9d744aa43e77babcbe53a1bef1160892e
ElahiAli <ali_elahi00@outlook.com>
2023-10-04 15:36:23 +0330

    init foundry with contract code

--- script/DeployVendingMachine.sol
+++ script/DeployVendingMachine.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {VendingMachine} from "../src/VendingMachine.sol";
+
+// import {HelperConfig} from "./HelperConfig.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
